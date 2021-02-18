52b5bb2c17dd4b0450d9db0a9b875a3e1de535be
Nikolaos Tsoukas <nick.tsoukas101@gmail.com>
2021-02-18 10:42:42 -0600

    chore: building header component

---
+++ components/Header.vue
@@ -0,0 +1,62 @@
+<template>
+    <div>
+      <section class="nav_bar flex items-center h-12 bg-black pl-8 ">
+          <!-- med and large nav contents -->
+          <div class="hidden md:flex items-center w-full">
+              <div class="text-white text-4xl">
+                  <h1>LOGO</h1>
+              </div>
+              <ul class="text-white w-full flex flex-grow justify-around">
+              <li>One</li>
+              <li>Two</li>
+              <li>Three</li>
+              <li>Four</li>
+          </ul>
+          </div>
+          <div class="flex md:hidden">
+              <img class="w-8" @click="tog" src="../static/menu.svg" alt="">
+          </div>
+      </section>
+      <!-- mobile nav -->
+      <section v-bind:class="{'closed' : isClosed}" class="bg-black text-white absolute h-full top-0 mob_nav pl-10 pt-4">
+         <img @click="tog" class="w-4 pb-8" src="../static/cancel.svg" alt="">
+          <ul>
+              <li>One</li>
+              <li>Two</li>
+              <li>Three</li>
+              <li>Four</li>
+          </ul>
+      </section>
+    </div>
+</template>
+
+<script>
+export default {
+   data(){
+       return {
+           isClosed: true
+       }
+   },
+   methods: {
+       tog(){
+           if(this.isClosed) {
+               this.isClosed = false
+           } else {
+               this.isClosed = true;
+           }
+       }
+   }
+}
+</script>
+
+<style scoped>
+    .mob_nav {
+        width: 200px;
+        transition: all .3s cubic-bezier(0, .52, 0, 1);
+    }
+    .closed {
+        margin-left: -200px;
+        transition: all .3s cubic-bezier(0, .52, 0, 1);
+    }
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="pl-4 mt-4">
+    <h1>This is the home page</h1>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+@apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
+
+</style>
